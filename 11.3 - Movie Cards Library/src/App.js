4b9578ed76b9d7bfc2a9fa2bcaa10305adce01ab
Pedro Barreto <pbdeoliveira@gmail.com>
2023-02-22 15:38:46 -0300

    feat: Movie Cards Library  exercise

---
+++ 11.3 - Movie Cards Library/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/Header';
+import movies from './data';
+import MovieList from './components/MovieList';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <MovieList movies={ movies } />
+    </div>
+  );
+}
+
+export default App;
---
+++ 11.3 - Movie Cards Library/src/components/MovieList.jsx
@@ -0,0 +1,30 @@
+// implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+      </div>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    imagePath: PropTypes.string,
+  })),
+};
+
+MovieList.defaultProps = {
+  movies: [],
+};
+
+export default MovieList;
