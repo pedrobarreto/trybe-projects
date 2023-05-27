fb72f3164ef5e61aa190ad262244aee7b1b69729
Pedro Barreto <pbdeoliveira@gmail.com>
2023-05-27 15:57:06 -0300

    feat: Stranger Things  exercise

---
+++ 26.3- Stranger Things/stranger-things-frontend/src/components/devTag.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class DevTag extends React.Component {
+  render() {
+    return (
+      <div className="devbar">
+        <h4>Em desenvolvimento</h4>
+      </div>
+    );
+  }
+}
+
+export default DevTag;
